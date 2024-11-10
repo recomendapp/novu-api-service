73a95aebfcb8d2915114d1ada8fef42d8afcfcca
loup <loup.quehec@gmail.com>
2024-11-10 13:05:51 +0100

    init

--- src/main.js
+++ src/main.js
@@ -5,16 +5,20 @@ import subscriber from './routes/subscriber/subscriber.controller.js';
 
 const app = new AppExpress();
 
+// Routes
 app.use('/subscriber', subscriber)
 
-// const getRoutes = (req, res) => {
-// 	res.json({
-// 		routes: [
-// 			'/',
-// 		]
-// 	});
-// };
 
-// app.get('/', getRoutes);
+// Default route
+const getRoutes = (req, res) => {
+	res.json({
+		routes: [
+			'/',
+			'/subscriber'
+		]
+	});
+};
+
+app.get('/', getRoutes);
 
 export default async (context) => await app.attach(context);
