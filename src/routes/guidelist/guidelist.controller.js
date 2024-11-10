24835e73d9ea7da4b381cda4f3363ac01e814e2d
loup <loup.quehec@gmail.com>
2024-11-11 00:08:57 +0100

    start guidelist notification

--- src/routes/guidelist/guidelist.controller.js
+++ src/routes/guidelist/guidelist.controller.js
@@ -6,17 +6,17 @@ const router = new AppExpress.Router();
 router.post("/", async (req, res, log, error) => {
 	try {
 		const payload = req.bodyJson;
-		log(`Payload: ${JSON.stringify(record)}`);
-		switch (payload.type) {
-			case "INSERT":
-				await sendGuidelist(payload);
-				break;
-			case "UPDATE":
-				await completeGuidelist(payload);
-				break;
-			default:
-				throw new Error("Invalid type");
-		}
+		log(`Payload: ${JSON.stringify(payload)}`);
+		// switch (payload.type) {
+		// 	case "INSERT":
+		// 		await sendGuidelist(payload);
+		// 		break;
+		// 	case "UPDATE":
+		// 		await completeGuidelist(payload);
+		// 		break;
+		// 	default:
+		// 		throw new Error("Invalid type");
+		// }
 		res.send("Guidelist sent");
 	} catch (err) {
 		error(err);
