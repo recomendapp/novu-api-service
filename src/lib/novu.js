8b364562f6b4d7106003d3b075b0fe692d21bb8f
loup <loup.quehec@gmail.com>
2024-11-10 14:40:37 +0100

    init

--- src/lib/novu.js
+++ src/lib/novu.js
@@ -1,8 +1,5 @@
 import { Novu } from "@novu/node";
 
-const novu = new Novu({
-	apiKey: process.env.NOVU_SECRET_KEY,
-	backendUrl: process.env.NOVU_BACKEND_URL || "https://eu.api.novu.co",
-});
+const novu = new Novu(process.env.NOVU_SECRET_KEY);
 
 export default novu;
