5eb469ece1bb18960340f4190ff2097607aa2742
loup <loup.quehec@gmail.com>
2024-11-10 14:19:17 +0100

    init

--- src/routes/subscriber/subscriber.controller.js
+++ src/routes/subscriber/subscriber.controller.js
@@ -17,41 +17,22 @@ router.get("/", async (req, res) => {
  */
 router.post("/", async (req, res, next) => {
   try {
-    // Show NOVU API KEY FROM ENV
-    console.log(process.env.NOVU_API_KEY);
+    const payload = req.bodyJson;
+    console.log(`Payload: ${JSON.stringify(payload)}`);
+    switch (payload.type) {
+      case "INSERT":
+        await createSubscriber(payload);
+        break;
+      case "UPDATE":
+        await updateSubscriber(payload);
+        break;
+      case "DELETE":
+        await deleteSubscriber(payload);
+        break;
+      default:
+        throw new Error("Invalid type");
+    }
     res.send("Subscriber created");
-  } catch (error) {
-    next(error);
-  }
-});
-
-/**
- * @swagger
- * /subscriber:
- *  put:
- *  summary: Update a subscriber
- * description: Update a subscriber
- * requestBody:
- * required: true
- */
-router.put("/", async (req, res, next) => {
-  try {
-    res.send("Subscriber updated");
-  } catch (error) {
-    next(error);
-  }
-});
-
-/**
- * @swagger
- * /subscriber:
- *  delete:
- * summary: Delete a subscriber
- * description: Delete a subscriber
- */
-router.delete("/", async (req, res, next) => {
-  try {
-    res.send("Subscriber deleted");
   } catch (error) {
     next(error);
   }
---
+++ src/routes/subscriber/subscriber.service.js
@@ -0,0 +1,67 @@
+import novu from '../../lib/novu.js';
+
+export const createSubscriber = async (payload) => {
+	const { schema, table, record } = payload;
+
+	const schemaTable = `${schema}.${table}`;
+	const subscriberId = record.id;
+
+	if (schemaTable === 'auth.users') {
+		await novu.subscribers.identify(subscriberId, {
+			email: record.email,
+			phone: record.phone
+		});
+	} else if (schemaTable === 'public.user') {
+		// Subscriber should already be identified (auth.users)
+		await novu.subscribers.update(subscriberId, {
+			avatar: record.avatar_url,
+			data: {
+				username: record.username,
+				full_name: record.full_name,
+			},
+			locale: record.language,
+		});
+	} else {
+		throw new Error('Invalid table');
+	}
+};
+
+export const updateSubscriber = async (payload) => {
+	const { schema, table, record } = payload;
+
+	const schemaTable = `${schema}.${table}`;
+	const subscriberId = record.id;
+
+	if (schemaTable === 'auth.users') {
+		await novu.subscribers.update(subscriberId, {
+			email: record.email,
+			phone: record.phone
+		});
+	} else if (schemaTable === 'public.user') {
+		await novu.subscribers.update(subscriberId, {
+			avatar: record.avatar_url,
+			data: {
+				username: record.username,
+				full_name: record.full_name,
+			},
+			locale: record.language,
+		});
+	} else {
+		throw new Error('Invalid table');
+	}
+};
+
+export const deleteSubscriber = async (payload) => {
+	const { schema, table, record } = payload;
+
+	const schemaTable = `${schema}.${table}`;
+	const subscriberId = record.id;
+
+	if (schemaTable === 'auth.users') {
+		await novu.subscribers.delete(subscriberId);
+	} else if (schemaTable === 'public.user') {
+		// Subscriber should already be deleted (auth.users)
+	} else {
+		throw new Error('Invalid table');
+	}
+};
